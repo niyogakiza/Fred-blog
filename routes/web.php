9f137bf5edc6bdc49b61080c02cb40ffa9dc94ae
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2020-01-11 16:39:05 +0000

    Admin profile and password settings

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ Auth::routes();
 Route::group(['as'=> 'admin.','prefix' => 'admin','namespace'=>'Admin', 'middleware'=>['auth', 'admin']], function () {
 
     Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+    Route::get('settings','SettingsController@index')->name('settings');
+    Route::put('profile-update','SettingsController@updateProfile')->name('profile.update');
+    Route::put('password-update','SettingsController@updatePassword')->name('password.update');
+
     Route::resource('tag', 'TagController');
     Route::resource('category','CategoryController');
     Route::resource('post','PostController');
