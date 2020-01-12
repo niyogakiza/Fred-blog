6ee233723d3eb2d63df930d1923e3fccf92959df
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2020-01-12 18:33:33 +0000

    All post details are done

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -73,6 +73,12 @@
                         <span>Pending Posts</span>
                     </a>
                 </li>
+                <li class="{{ Request::is('admin/comments') ? 'active' : '' }}">
+                    <a href="{{ route('admin.comment.index') }}">
+                        <i class="material-icons">comment</i>
+                        <span>Comments</span>
+                    </a>
+                </li>
                 <li class="{{ Request::is('admin/subscriber') ? 'active' : '' }}">
                     <a href="{{ route('admin.subscriber.index') }}">
                         <i class="material-icons">subscriptions</i>
@@ -116,6 +122,12 @@
                     <a href="{{ route('author.favorite.index') }}">
                         <i class="material-icons">favorite</i>
                         <span>Favorite Posts</span>
+                    </a>
+                </li>
+                <li class="{{ Request::is('author/comments') ? 'active' : '' }}">
+                    <a href="{{ route('author.comment.index') }}">
+                        <i class="material-icons">comment</i>
+                        <span>Comments</span>
                     </a>
                 </li>
                 <li class="header">System</li>
